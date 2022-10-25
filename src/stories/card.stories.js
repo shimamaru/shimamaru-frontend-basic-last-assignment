3ab800bc10e708f8b5890cd330e4f740fd3fc054
shimamaru <115210019+shimamaru@users.noreply.github.com>
2022-10-25 21:19:15 +0900

    planの作成

---
+++ src/stories/card.stories.js
@@ -0,0 +1,8 @@
+import item_card from "../components/molecules/card/card.html";
+import "../components/molecules/card/card.scss";
+
+export default {
+  title: "molecules/card"
+}
+
+export const card = () => item_card;

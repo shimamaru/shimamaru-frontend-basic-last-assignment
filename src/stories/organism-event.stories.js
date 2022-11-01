9e865652388c7bb569f3b8b605844e6430da3cdc
shimamaru <115210019+shimamaru@users.noreply.github.com>
2022-11-01 23:44:39 +0900

    organisms/newsの作成

---
+++ src/stories/organism-event.stories.js
@@ -0,0 +1,6 @@
+import item_news from "../components/organisms/news/news.html";
+import "../components/organisms/news/news.scss";
+
+export default { title: "organisms/news" };
+
+export const news = () => item_news;

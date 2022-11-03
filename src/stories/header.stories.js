710c06c86f6be6f79c444677dae47dd0ab8462da
shimamaru <115210019+shimamaru@users.noreply.github.com>
2022-11-03 20:20:32 +0900

    ヘッダーの作成途中

---
+++ src/stories/header.stories.js
@@ -0,0 +1,8 @@
+import item_header from "../components/organisms/header/header.html";
+import "../components/organisms/header/header.scss";
+
+export default {
+  title: "organisms/header"
+}
+
+export const header = () => item_header;

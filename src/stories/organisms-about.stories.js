2c85ef06f62761cc3d4043a3ad4969160c20d556
shimamaru <115210019+shimamaru@users.noreply.github.com>
2022-11-06 19:56:18 +0900

    organisms/aboutの作成

---
+++ src/stories/organisms-about.stories.js
@@ -0,0 +1,8 @@
+import item_about from "../components/organisms/about/about.html";
+import "../components/organisms/about/about.scss";
+
+export default {
+  title: "organisms/about"
+}
+
+export const about = () => item_about;

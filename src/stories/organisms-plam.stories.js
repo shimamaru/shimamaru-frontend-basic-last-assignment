5b63563c9a0d293f93f052bb198c373accded81f
shimamaru <115210019+shimamaru@users.noreply.github.com>
2022-11-14 08:56:17 +0900

    レイアウトの空白の修正

---
+++ src/stories/organisms-plam.stories.js
@@ -0,0 +1,8 @@
+import item_plans from "../components/organisms/plan/plan.html";
+import "../components/organisms/plan/plan.scss";
+
+export default {
+  title: "organisms/plan"
+}
+
+export const plans = () => item_plans;

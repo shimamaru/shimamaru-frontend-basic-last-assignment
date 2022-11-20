473d2237aa3d95ccfed06c2ab7752b1699afcfc7
shimamaru <115210019+shimamaru@users.noreply.github.com>
2022-11-20 16:53:58 +0900

    PLANの修正

--- src/stories/molecules-plan.stories.js
+++ src/stories/molecules-plan.stories.js
@@ -1,6 +1,6 @@
 import item_plan from "../components/molecules/plan/plan.html";
 import "../components/molecules/plan/plan.scss";
 
-export default { title: "molecules/plan" }
+export default { title: "molecules/plan" };
 
 export const Example = () => item_plan;
--- src/stories/organisms-plam.stories.js
+++
@@ -1,8 +0,0 @@
-import item_plans from "../components/organisms/plan/plan.html";
-import "../components/organisms/plan/plan.scss";
-
-export default {
-  title: "organisms/plan"
-}
-
-export const plans = () => item_plans;

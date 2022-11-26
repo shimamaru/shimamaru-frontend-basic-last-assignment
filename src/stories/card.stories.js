0cc2174ddb228571545ed2cd64bae6f66aa1c1c1
shimamaru <115210019+shimamaru@users.noreply.github.com>
2022-11-26 09:56:39 +0900

    ファイルの削除

--- src/stories/card.stories.js
+++
@@ -1,8 +0,0 @@
-import item_card from "../components/molecules/card/card.html";
-import "../components/molecules/card/card.scss";
-
-export default {
-  title: "molecules/card"
-}
-
-export const card = () => item_card;

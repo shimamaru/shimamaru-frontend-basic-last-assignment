ab9bba63601cd21758d46e7cd67f300059bbd1ee
shimamaru <115210019+shimamaru@users.noreply.github.com>
2022-11-28 13:19:21 +0900

    不要なファイルの削除

--- src/stories/header-re.stories.js
+++
@@ -1,8 +0,0 @@
-import item_header from "../components/organisms/header-re/header-re.html";
-import "../components/organisms/header-re/header-re.scss";
-
-export default {
-  title: "organisms/header",
-};
-
-export const header = () => item_header;

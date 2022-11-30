830045650ded0f260ac92fdcbea564ffe3d0ba39
shimamaru <115210019+shimamaru@users.noreply.github.com>
2022-11-30 13:06:27 +0900

    テンプレートの作成

---
+++ src/stories/templates-NewsAndEvents.stories.js
@@ -0,0 +1,8 @@
+import item_NewsAndEvents from "../components/templates/NewsAndEvents.html";
+import "../components/templates/NewsAndEvents.scss";
+
+export default {
+  title: "templates/NewsAndEvents",
+};
+
+export const NewsAndEvents = () => item_NewsAndEvents;

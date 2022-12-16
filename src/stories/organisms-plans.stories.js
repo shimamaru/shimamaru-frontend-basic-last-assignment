78a2b1678abb95374c13a4ca0dea3179b6afb129
shimamaru <115210019+shimamaru@users.noreply.github.com>
2022-12-16 11:38:25 +0900

    organisms/plansの作成

---
+++ src/stories/organisms-plans.stories.js
@@ -0,0 +1,8 @@
+import item_plans from "../components/plans/plans.html";
+import "../components/plans/plans.scss";
+
+export default {
+  title: "organisms/plans",
+};
+
+export const plans = () => item_plans;
